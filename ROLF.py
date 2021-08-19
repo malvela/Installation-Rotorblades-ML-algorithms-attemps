690d48bad811c465bd970f505d9168e9cb443d3a
sascha1 <sascha1@uni-bremen.de>
2021-08-19 17:03:51 +0200

    updating files

--- ROLF.py
+++ ROLF.py
@@ -1,16 +1,12 @@
 #%%
 
-import datasets as datasets
-import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn import datasets
-import matplotlib.cm as cm
 import time
-from random import randint
-from sklearn.datasets import make_blobs
 import numpy as np
 import pandas as pd
+
 
 #%%
 
@@ -108,43 +104,51 @@ class ROLF():
             return self.lables[acceptors[0]]
         else:
             return -1
+    def savemodel(self):
+        np.savetxt('ROLFcenter.csv', self.center, delimiter=',')
+        np.savetxt('ROLFlables.csv', self.lables, delimiter=',')
 
+        '''
+        df = pd.DataFrame()
+        df['labels'] = self.lables
+        df['centers0'] = self.center[0]
+        df['p'] = self.p
+        df['lr_center'] = self.lr_center
+        df['lr_sigma'] = self.lr_sigma
+        df['initSigma'] = self.initSigma
+        print(df.head(5))
+        '''
 #%%
 
 # Run the examples on your own, replace make_circles with make_moons to change the example
 # If you get to much cluster or to less, play around with the initSigma
 
-X = pd.read_csv('Results_preprocessing/turbine06/sbi1_turbine6.csv', delimiter = ',')
-
-Z,y = datasets.make_moons(n_samples=len(X), noise=0.05)
+X = pd.read_csv('hammerhead_reduced.csv', delimiter = ',')
+X = X.iloc[0:1000, :]
+X = X.fillna(0)
 del X['epoch']
-X = PCA(n_components=10).fit_transform(X)
-print(X.shape)
-rolf = ROLF(p=2, lr_center=0.05, lr_sigma=0.05, initSigma=1,strategy = 'max')
+#X = PCA(n_components=2).fit_transform(X)
+Z,y = datasets.make_moons(n_samples=100000, noise=0.05)
+rolf = ROLF(p=1.5,lr_center = 0.01, lr_sigma = 0.01,initSigma = 10, strategy = 'max')
 start_time = time.time()
 rolf.fit(X)
-
-
+print(type(rolf.center))
+print(type(rolf.lables))
 print('')
 print("fitting ROLF took %s seconds" % (time.time() - start_time))
 print(len(rolf.center), 'neurons approximating', len(X), 'datapoints')
 print(len(np.unique(rolf.lables)), 'clusters where found')
-
-print(rolf.center)
-print(rolf.center.shape)
-print(rolf.lables)
-
-
-silhouette_score(rolf.center, rolf.lables)
-####### Vizualizing learned Network ###############################################
+print(silhouette_score(rolf.center, rolf.lables))
 '''
+##### Vizualizing learned Network ###############################################
+
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
 for c in range(0, len(rolf.center)):
     radius = rolf.sigma[c]*rolf.p
-    circle = plt.Circle((rolf.center[c][0], rolf.center[c][1]),radius, alpha=0.15, color=colors[rolf.lables[c]])
+    circle = plt.Circle((rolf.center[c][0], rolf.center[c][1]),radius, alpha=0.15,)
     ax1.add_patch(circle)
-    circle = plt.Circle((rolf.center[c][0], rolf.center[c][1]),rolf.sigma[c], alpha=0.25, color=colors[rolf.lables[c]])
+    circle = plt.Circle((rolf.center[c][0], rolf.center[c][1]),rolf.sigma[c], alpha=0.25,)
     ax1.add_patch(circle)
 
     for k in range(0, len(rolf.center)):
@@ -153,7 +157,7 @@ for c in range(0, len(rolf.center)):
         if distance <= perc:
             ax1.plot([rolf.center[c][0], rolf.center[k][0]], [
                     rolf.center[c][1], rolf.center[k][1]], linewidth=1, color='black')
-ax1.scatter(X[:, 0], X[:, 1],X[:, 2], marker='.', color="black", alpha=0.02)
+ax1.scatter(X[:, 0], X[:, 1], marker='.', color="black", alpha=0.02)
 ax1.set_title("network vizualization")
 
 ##### Vizualize input space clustering #############################################
@@ -161,8 +165,8 @@ ax1.set_title("network vizualization")
 
 labels = []
 points = []
-for x in np.arange(-10,10,0.1):
-    for y in np.arange(-10,10,0.1):
+for x in np.arange(-50,50,1):
+    for y in np.arange(-20,20,1):
         points.append([x,y])
         labels.append(rolf.predict([x,y]))
 points = np.asarray(points)
@@ -175,8 +179,38 @@ for i in np.unique(labels):
     else:
         ax2.scatter(cluster[:,0],cluster[:,1],label='class: ' + str(i))
 
-ax2.legend()
 ax2.set_title("predicted clusters for the input space")
 plt.show()
-
 '''
+'''
+#3D Plots
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+labels = []
+points = []
+for x in np.arange(-50,50,0.1):
+    for y in np.arange(-20,20,0.1):
+        for z in np.arange(-20, 20, 0.1):
+            print(type(x))
+            print(type(y))
+            print(type([x,y,z]))
+            points.append([x,y,z])
+            labels.append(rolf.predict([x,y,z]))
+points = np.asarray(points)
+print(points)
+labels = np.asarray(labels)
+print(labels)
+for i in np.unique(labels):
+    cluster = points[labels==i]
+    if i == -1:
+        plt.scatter(cluster[:,0],cluster[:,1],cluster[:,2],label='class: unknown')
+    else:
+        plt.scatter(cluster[:,0],cluster[:,1],cluster[:,2],label='class: ' + str(i))
+
+plt.title("predicted clusters for the input space")
+plt.show()
+
+#plotting
+'''
+
